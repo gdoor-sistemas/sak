5d279a8a6ad18a7005e1cb93886438f61fc598a6
Leandro Hermes <leandro@leandrohermes.net>
2020-09-22 15:13:31 -0300

    chore(app): adds scoped import to main module

--- projects/sak/src/app/app.module.ts
+++ projects/sak/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PipesModule } from '@gdoor/helpers';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PipesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
