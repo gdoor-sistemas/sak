46c0e8a92cf6965ef63079e4f846dfae3c6b9198
Leandro Hermes Neto <leandro@leandrohermes.net>
2020-10-08 15:59:38 -0300

    feat: adds validation helper class

---
+++ projects/helpers/src/lib/validate.ts
@@ -0,0 +1,51 @@
+import { Str } from './str.helper';
+
+export class Validate {
+  /**
+   *
+   */
+  public static cpf(value: string): boolean {
+    if (value.replace(/[^0-9\.-]/g, '') !== value) {
+      return false;
+    }
+
+    value = Str.onlyNumbers(value);
+
+    if (value.length !== 11 || !Array.from(value).filter(e => e !== value[0]).length) {
+      return false;
+    }
+
+    let sum = 0;
+    let rest;
+
+    if (value === '00000000000') {
+      return false;
+    }
+
+    for (let i = 1; i <= 9; i++) {
+      sum = sum + +value.substring(i - 1, i) * (11 - i);
+    }
+
+    rest = (sum * 10) % 11;
+
+    if ((rest === 10) || (rest === 11)) {
+      rest = 0;
+    }
+
+    if (rest !== +value.substring(9, 10)) {
+      return false;
+    }
+
+    sum = 0;
+    for (let i = 1; i <= 10; i++) {
+      sum = sum + +value.substring(i - 1, i) * (12 - i);
+    }
+    rest = (sum * 10) % 11;
+
+    if ((rest === 10) || (rest === 11)) {
+      rest = 0;
+    }
+
+    return rest === +value.substring(10, 11);
+  }
+}
