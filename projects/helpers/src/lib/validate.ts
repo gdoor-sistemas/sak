6ffba180ec99e8655c383d4408255840dd5aec87
Leandro Hermes Neto <leandro@leandrohermes.net>
2020-10-09 08:51:33 -0300

    feat: adds method for validating cnpj codes

--- projects/helpers/src/lib/validate.ts
+++ projects/helpers/src/lib/validate.ts
@@ -3,7 +3,7 @@ export class Validate {
    *
    */
   public static cpf(value: string): boolean {
-    if (value.replace(/[^\d\.-]/g, '') !== value) {
+    if (value.replace(/[^\d\.\-]/g, '') !== value) {
       return false;
     }
 
@@ -15,10 +15,6 @@ export class Validate {
 
     let sum = 0;
     let rest;
-
-    if (value === '00000000000') {
-      return false;
-    }
 
     for (let i = 1; i <= 9; i++) {
       sum = sum + +value.substring(i - 1, i) * (11 - i);
@@ -45,5 +41,52 @@ export class Validate {
     }
 
     return rest === +value.substring(10, 11);
+  }
+
+  /**
+   *
+   */
+  public static cnpj(value: string): boolean {
+    if (value.replace(/[^\d\.\-\/]/g, '') !== value) {
+      return false;
+    }
+
+    value = value.replace(/[^\d]/g, '');
+
+    if (value.length !== 14 || !Array.from(value).filter(e => e !== value[0]).length) {
+      return false;
+    }
+
+    // validate verifier digits
+    let size = value.length - 2;
+    let numbers = value.substring(0, size);
+    const verifiers = value.substring(size);
+    let soma = 0;
+    let pos = size - 7;
+
+    for (let i = size; i >= 1; i--) {
+      soma += +numbers.charAt(size - i) * pos--;
+      if (pos < 2) {
+        pos = 9;
+      }
+    }
+
+    let result = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (result !== +verifiers.charAt(0)) {
+      return false;
+    }
+
+    size = size + 1;
+    numbers = value.substring(0, size);
+    soma = 0;
+    pos = size - 7;
+    for (let i = size; i >= 1; i--) {
+      soma += +numbers.charAt(size - i) * pos--;
+      if (pos < 2) {
+        pos = 9;
+      }
+    }
+    result = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    return result === +verifiers.charAt(1);
   }
 }
