d43de45f49e4a4b07f51488264447a523a67258c
Leandro Hermes Neto <leandro@leandrohermes.net>
2021-01-11 14:43:27 -0300

    fix(sak): fixes import

--- projects/sak/src/app/helpers/home/helpers-home.component.ts
+++ projects/sak/src/app/helpers/home/helpers-home.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { DestroyerService } from '@gdoor/helpers/services';
+import { ReleaseOnDestroy } from '@gdoor/helpers/services';
 
 @Component({
   selector: 'sak-helpers-home',
   templateUrl: './helpers-home.component.html',
   styleUrls: ['./helpers-home.component.scss'],
-  providers: [DestroyerService],
+  providers: [ReleaseOnDestroy],
 })
 export class HelpersHomeComponent implements OnInit {
 
-  constructor(private _destroyed$: DestroyerService) { }
+  constructor(private _destroyed$: ReleaseOnDestroy) { }
 
   public ngOnInit(): void {
   }
