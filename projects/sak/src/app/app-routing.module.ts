7290ce73a7f3feff50c54c5817a3e82cbc325f06
Leandro Hermes Neto <leandro@leandrohermes.net>
2021-02-22 10:55:16 -0300

    chore: updates Angular to v11

--- projects/sak/src/app/app-routing.module.ts
+++ projects/sak/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: 'helpers', loadChildren: () => import('./helpers/helpers.module').then(m => m.HelpersModule)},
@@ -7,7 +7,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
