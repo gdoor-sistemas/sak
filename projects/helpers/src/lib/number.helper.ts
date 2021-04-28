4e1d49ff467fa7900c8bd0186962e2b0599b85e4
Leandro Hermes Neto <leandro@leandrohermes.net>
2021-04-28 11:56:03 -0300

    feat(helpers): adds functions to Number helper

--- projects/helpers/src/lib/number.helper.ts
+++ projects/helpers/src/lib/number.helper.ts
@@ -9,4 +9,20 @@ export class Number {
     const factor = Math.pow(10, precision);
     return Math.round(value * factor) / factor;
   }
+
+  /**
+   * Returns if a `needle` is between `start` and `end`. Returns `true` if the value is equals to `start` or `end` params.
+   */
+  public static isBetween(needle: number, start: number, end: number): boolean {
+    return (needle >= start) && (needle <= end);
+  }
+
+  /**
+   * Syntactic sugar for returning opposite value of #isBetween.
+   *
+   * @see Number.isBetween
+   */
+  public static isNotBetween(needle: number, start: number, end: number): boolean {
+    return !this.isBetween.apply(null, arguments);
+  }
 }
--- projects/helpers/src/test/number.helper.spec.ts
+++ projects/helpers/src/test/number.helper.spec.ts
@@ -12,4 +12,20 @@ describe('Number helper', () => {
   it('should round with precision=3', () => {
     expect(Number.rMoney(1.2358, 3)).toBe(1.236);
   });
+
+  it('should check if a number is between other 2', () => {
+    expect(Number.isBetween(1, 0, 2)).toBe(true, '1, 0, 2');
+    expect(Number.isBetween(0, 0, 2)).toBe(true, '0, 0, 2');
+    expect(Number.isBetween(2, 0, 2)).toBe(true, '2, 0, 2');
+    expect(Number.isBetween(-1, 0, 2)).toBe(false, '-1, 0, 2');
+    expect(Number.isBetween(3, 0, 2)).toBe(false, '3, 0, 2');
+  });
+
+  it('should check if a number is not between other 2', () => {
+    expect(Number.isNotBetween(1, 0, 2)).toBe(false, '1, 0, 2');
+    expect(Number.isNotBetween(0, 0, 2)).toBe(false, '0, 0, 2');
+    expect(Number.isNotBetween(2, 0, 2)).toBe(false, '2, 0, 2');
+    expect(Number.isNotBetween(-1, 0, 2)).toBe(true, '-1, 0, 2');
+    expect(Number.isNotBetween(3, 0, 2)).toBe(true, '3, 0, 2');
+  });
 });
