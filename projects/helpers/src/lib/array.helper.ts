34341e77918e6e575894f3d9f2cc69aa9c39755b
Leandro Hermes Neto <leandro@leandrohermes.net>
2021-11-05 01:03:07 -0300

    feat: adds array chunk helper

--- projects/helpers/src/lib/array.helper.ts
+++ projects/helpers/src/lib/array.helper.ts
@@ -22,4 +22,17 @@ export class ArrayHelper {
   public static distinct<T = any>(array: T[]): T[] {
     return array.filter((value, currIndex, self) => self.indexOf(value) === currIndex);
   }
+
+  /**
+   * Generates a list chunked by the given size.
+   */
+  public static chunk<T>(array: T[], chunkSize: number): T[][] {
+    const result = [];
+
+    for (let i = 0; i < array.length; i += chunkSize) {
+      result.push(array.slice(i, i + chunkSize));
+    }
+
+    return result;
+  }
 }
